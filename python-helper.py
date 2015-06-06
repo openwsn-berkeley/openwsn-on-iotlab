bec581aac8ac4b0cf17965a4c5f7162d97a49398
Nicola Accettura <nick.accettura@gmail.com>
2015-06-06 00:57:50 -0700

    running openvisualizer

--- python-helper.py
+++ python-helper.py
@@ -18,7 +18,9 @@ import subprocess
     # f.write('python openVisualizerWeb.py --port 1234 --iotlabmotes '+','.join(nodes))
         
 def main():
-    print 'checking nodes available'
+    print '+--------------------------------------+'
+    print '|      checking available nodes        |'
+    print '+--------------------------------------+'
     nodes_info=subprocess.check_output(['experiment-cli','info','--site','rennes','-li'])
     nodes_alive=eval(nodes_info)['items'][0]['rennes']['wsn430']['Alive']
     nodes_alive=reduce(lambda x,y:x+y,[range(int(nodes_consecutive.split('-')[0]),int(nodes_consecutive.split('-')[-1])+1) for nodes_consecutive in nodes_alive.split('+')])
@@ -35,13 +37,24 @@ def main():
                 nodes_alive.pop(0)
     nodes_to_reserve_string='+'.join([str(node) for node in nodes_selected])
     firmware_path='~/openwsn/openwsn-fw/build/wsn430v14_mspgcc/projects/common/03oos_openwsn_prog.ihex'
-    print 'reserving nodes'
+    print '+--------------------------------------+'
+    print '|          reserving nodes             |'
+    print '+--------------------------------------+'
     experiment_id=subprocess.check_output(['experiment-cli','submit','-n','minimaltest','-d','30','-l','rennes,wsn430,{0},{1}'.format(nodes_to_reserve_string,firmware_path)])
     experiment_id=eval(experiment_id)['id']
-    print 'waiting experiment to start', experiment_id
-    subprocess.check_output(['experiment-cli','wait','-i','{}'.format(experiment_id)])
-    print 'experiment running', experiment_id
-    print
+    print '+--------------------------------------+'
+    print '|     waiting to start experiment      |'
+    print '+--------------------------------------+'
+    subprocess.call(['experiment-cli','wait','-i','{}'.format(experiment_id)])
+    print '+--------------------------------------+'
+    print '|       running Openvisualizer         |'
+    print '+--------------------------------------+'
+    subprocess.call(['cd','~/openwsn/openwsn-sw/software/openvisualizer/bin/openVisualizerApp/'])
+    subprocess.call(['python','openVisualizerWeb.py','--port','1234','--iotlabmotes','+'.join([str(node) for node in nodes_selected])])
+    print '+--------------------------------------+'
+    print '|          delete experiment           |'
+    print '+--------------------------------------+'
+    subprocess.call(['experiment-cli','stop','-i','{}'.format(experiment_id)])
     
 #============================ main ============================================
 
