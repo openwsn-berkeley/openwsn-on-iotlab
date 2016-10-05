c3317d5877f28a979a055e4b379a84e65317bf50
changtengfei <tengfei.chang@gmail.com>
2016-10-05 17:31:04 +0200

    FW-471. script for plotting.

---
+++ A8/parser/plot.py
@@ -0,0 +1,126 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import string
+import scipy as sp
+import scipy.stats
+
+import os
+import traceback
+
+import ast
+
+FIGUREFILE_PATH           = './'
+CELLTYPE_OFF              = 0
+CELLTYPE_TX               = 1
+CELLTYPE_RX               = 2
+CELLTYPE_TXRX             = 3
+
+class plotFigure():
+    def __init__(self):
+        self.figureData = {}
+        self.getFigureData()
+        self.plotFigures()
+        
+    def getFigureData(self):
+        for filename in os.listdir(FIGUREFILE_PATH):
+            if filename.endswith('.txt'):
+                print 'Getting data {0}...'.format(filename)
+                with open(filename,'r') as f:
+                    s = f.read()
+                    self.figureData[filename] = ast.literal_eval(s)
+                print 'Done.'
+                
+    def plotFigures(self):
+        for filename in os.listdir(FIGUREFILE_PATH):
+            if filename.endswith('.txt'):
+                self.plotOneFigure(filename)
+        plt.show()
+                
+    def plotOneFigure(self,filename):
+        if filename == 'figure_1_schedule&rank.txt':
+            self.plotCellsVSRankData()
+        elif filename == 'figure_2_syncTime.txt':
+            self.plotSynctimeVSNumberMotes()
+        elif filename == 'figure_3_cellUsage.txt':
+            self.plotCellUsageVSNumberCells()
+        elif filename == 'figure_4_cellPDR.txt':
+            self.plotCellPDR()
+        
+    def plotCellsVSRankData(self):
+        plt.figure(1)
+        xData = []
+        yData = []
+        for moteid, data in self.figureData['figure_1_schedule&rank.txt'].items():
+            numberOfCell = 0
+            for i in range(23): # 23 cells buffer entries 
+                if data[i]['type'] == CELLTYPE_TX or data[i]['type'] == CELLTYPE_RX:
+                    numberOfCell += 1
+            xData += [data['myDAGrank']['myDAGrank']]
+            yData += [numberOfCell]
+        plt.plot(xData,yData,'*')
+        plt.grid(True)
+        plt.xlabel('DAGRank')
+        plt.ylabel('Number Of Cells')
+        plt.title('number cells VS DAGRank')
+        
+    def plotSynctimeVSNumberMotes(self):
+        plt.figure(2)
+        xData = []
+        yData = []
+        asn = []
+        for moteid, data in self.figureData['figure_2_syncTime.txt'].items():
+            if 'asn_0_1' in data:
+                asn += [data['asn_0_1']*0.015]
+            else:
+                continue
+        xData = sorted(asn)
+        yData = [i+1 for i in range(len(asn))]
+        plt.plot(xData,yData,'*')
+        plt.grid(True)
+        plt.xlabel('Time (Second)')
+        plt.ylabel('Number Of Motes')
+        plt.title('sync time')
+        
+    def plotCellUsageVSNumberCells(self):
+        plt.figure(3)
+        xData = []
+        yData = []
+        for moteid, data in self.figureData['figure_3_cellUsage.txt'].items():
+            for item in data:
+                xData += [item[0]]
+                yData += [item[1]]
+            plt.plot(xData,yData)
+        plt.grid(True)
+        plt.xlabel('SlotFrame Number')
+        plt.ylabel('Cell Usage (Number of transmission in last 10 Slotframes)')
+        plt.title('Cell usage per slotframe')
+        plt.legend()
+        
+    def plotCellPDR(self):
+        plt.figure(4)
+        xData = [i for i in range(101)]
+        yData = {}
+        for moteid, data in self.figureData['figure_4_cellPDR.txt'].items():
+            plt.plot(xData,data,'*')
+        plt.grid(True)
+        plt.xlabel('SlotOffset')
+        plt.ylabel('Cell Packet Delivery Ratio')
+        plt.title('Cell Packet Delivery Ratio')
+        
+#============= public ===================
+                
+
+
+def main():
+    try:
+        plotFigure()
+        raw_input("Script ended normally. Press Enter to close.")
+    except Exception as err:
+        print traceback.print_exc()
+        raw_input("Script CRASHED. Press Enter to close.")
+        
+# ================= main fucntion ==========================
+if __name__ == '__main__':
+    main()
+    
